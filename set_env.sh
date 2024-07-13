5ca9b7383174d41306e8d7c5afd6c492bef7986b
Mohamed Hassan <mohamedhassan@xnmh7nhyyg.localdomain>
2024-07-13 23:23:56 +0200

    decouble the api services

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
+# psql --host=udagramdb.cxuy8w244e1n.us-east-1.rds.amazonaws.com --port=5432 --username=postgres --dbname=postgres
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=password
+export POSTGRES_HOST=udagramdb.cxuy8w244e1n.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=udagram-654654244606-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
