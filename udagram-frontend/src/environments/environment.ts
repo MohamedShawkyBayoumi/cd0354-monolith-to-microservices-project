16459c69d9c823531b09e20c84f9245dbc053bac
Mohamed Hassan <mohamedhassan@xnmh7nhyyg.localdomain>
2024-07-17 16:04:15 +0200

    add k8s files

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,10 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  // apiHost: "http://localhost:8080/api/v0",
+  apiHost:
+    "http://af7cd0ec0532148078ccb8859c2ac958-2117080380.us-east-1.elb.amazonaws.com/api/v0",
 };
 
 /*
